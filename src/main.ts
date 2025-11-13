ea6a91b0bf10ad3d4f65faaceabc954025bc2e4e
Oleksandr <ku@grade.ua>
2025-11-13 17:45:19 +0200

    Add global styles and theme support for light and dark modes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './app/App.vue'
 import { withProviders } from './app/providers'
+import '@/shared/styles/global.css'
 
 withProviders(createApp(App)).mount('#app')
